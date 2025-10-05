8b9d2affcc016f64dc5e4c58ac3609f0101cbc41
VISHAL CHAURASIYA <Vishal-GitLab>
2025-10-05 23:19:38 +0530

    Main

--- src/src/BasicJava/Main.java
+++ src/src/BasicJava/Main.java
@@ -1,7 +1,7 @@
 package BasicJava;
 
-public class main {
-    public static void main(String[] args) {
-        System.out.println("sdfghjkl;");
+public class Main {
+    public static void main(String[] args) {   // this class access can we any where ,, class --> name of group properties and function ,, main --> this is the name of the file and main function is the entry point of the java,, String[] arg -->> this is the comond line of the argument and string - string the collection of the string
+        System.out.println("Hello world");     // public --> The method (or class, variable, etc.) is accessible from anywhere,,, static -->>it belongs to the class, not to any specific object,, void -->> void is the return of the function main --> without main  function is code run
     }
 }
