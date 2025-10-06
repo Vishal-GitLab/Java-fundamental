23b880533e99e0f85b82dca5d87c51aa30eafd99
VISHAL CHAURASIYA <Vishal-GitLab>
2025-10-06 19:26:14 +0530

    Basic

--- src/src/BasicJava/Main.java
+++ src/src/BasicJava/Main.java
@@ -1,5 +1,5 @@
 package BasicJava;
-
+     // package --> in simle tern                                                                          
 public class Main {
     public static void main(String[] args) {   // this class access can we any where ,, class --> name of group properties and function ,, main --> this is the name of the file and main function is the entry point of the java,, String[] arg -->> this is the comond line of the argument and string - string the collection of the string
         System.out.println("Hello world");
