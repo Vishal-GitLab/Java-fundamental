ee6ccd9d67aae69dcf688c95c83520681df1a3c9
St. Vishal Chusasiya <vishal_246045@saitm.ac.in>
2025-10-08 16:32:58 +0530

    Add files via upload

---
+++ src/src/BasicJava/Primitives.java
@@ -0,0 +1,20 @@
+package BasicJava;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 56;          // it is 4 byte
+
+
+        // String is not Primitive,,,   primitive --- >>  this last data type in to any other data type, its known as primitive data type
+//        String name = "Vishal Chaurasiya";            // Strng start is double cots
+//        System.out.println(name);
+
+
+        char letter = 'V';          // char is start single cots   ,, it is 4 byte
+        float marks = 98.88f;             // this is 4 byte
+        double largeDecimalNumbers = 384675.787;       // this is 8 byte
+        long largeInteger = 84652784627L;            // this is 8  yte
+        boolean check = false;
+
+    }
+}
