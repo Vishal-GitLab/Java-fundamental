8bbe297ea4358bc552bb093ec1b4a36d67ad57aa
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-10-15 21:27:50 +0530

    Add files via upload

---
+++ src/src/Conditional/Main.java
@@ -0,0 +1,47 @@
+package Conditional;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        /*
+        Syntax of if statement :
+        if (boolean expression T or F) {
+        // body
+        } else {
+        // do this
+        }
+         */
+
+
+        int salary = 25000;
+//        if (salary > 10000) {
+//            salary = salary + 2000;
+//        } else {
+//            salary = salary + 1000;
+       // }
+
+
+        if (salary > 10000 ) {
+            salary += 2000;
+        } else  if (salary > 20000) {
+            salary += 3000;
+        }else {
+            salary += 1000;
+        }
+
+        System.out.println(salary);
+    }
+}
+
+
+
+
+
+
+
+
+
+
