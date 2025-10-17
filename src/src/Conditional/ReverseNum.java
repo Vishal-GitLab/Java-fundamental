d91df5653a730e6908e9b8cc8f3a7d6680fd6b3a
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-10-17 22:16:15 +0530

    Add files via upload

---
+++ src/src/Conditional/ReverseNum.java
@@ -0,0 +1,32 @@
+package Conditional;
+
+
+public class ReverseNum {
+    public static void main(String[] args) {
+
+        int num =  3567;
+
+        int ans = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+
+            ans = ans * 10 + rem;
+        }
+        System.out.println(ans);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
