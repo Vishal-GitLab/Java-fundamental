db57dcd3115b88bbbd3f73f8f42d65837f8f74dc
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-10-21 23:03:43 +0530

    Refactor switch statement with arrow syntax
    Updated switch statement syntax to use arrow case labels.

--- src/src/Switch/Basics.java
+++ src/src/Switch/Basics.java
@@ -7,19 +7,45 @@ public class Basics {
         Scanner input = new Scanner(System.in);
         String fruit = input.next();
 
+//        switch (fruit) {
+//            case "Mango":
+//                System.out.println("king of fruit");
+//                break;
+//            case "Apple":
+//                System.out.println("A sweet red fruit");
+//                break;
+//
+//            case "Grapes":
+//                System.out.println("small fruit");
+//
+//            default:
+//                System.out.println("Please enter a valid fruit");
+//        }
+
         switch (fruit) {
-            case "Mango":
-                System.out.println("king of fruit");
-                break;
-            case "Apple":
-                System.out.println("A sweet red fruit");
-                break;
-
-            case "Grapes":
-                System.out.println("small fruit");
-
-            default:
-                System.out.println("Please enter a valid fruit");
+            case "Mango" -> System.out.println("King of fruit");
+            case "Apple" -> System.out.println("A sweet red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            case "Grapes" -> System.out.println("small fruit");
+            default -> System.out.println("Please enter a valid fruit");
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
