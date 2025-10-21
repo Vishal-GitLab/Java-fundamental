8ba6ff88fae4a13eb88904ab41ddc4822185994b
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-10-21 22:40:17 +0530

    Add files via upload

---
+++ src/src/Switch/Basics.java
@@ -0,0 +1,25 @@
+package Switch;
+
+import java.util.Scanner;
+
+public class Basics {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String fruit = input.next();
+
+        switch (fruit) {
+            case "Mango":
+                System.out.println("king of fruit");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+
+            case "Grapes":
+                System.out.println("small fruit");
+
+            default:
+                System.out.println("Please enter a valid fruit");
+        }
+    }
+}
