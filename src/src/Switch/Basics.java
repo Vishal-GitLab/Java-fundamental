e41c150dcd3595b86aaefc841e5c597d65702fd0
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-10-22 23:02:15 +0530

    Update Basics.java

--- src/src/Switch/Basics.java
+++ src/src/Switch/Basics.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Basics {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String fruit = input.next();
+//        String fruit = input.next();
 
 //        switch (fruit) {
 //            case "Mango":
@@ -22,15 +22,42 @@ public class Basics {
 //                System.out.println("Please enter a valid fruit");
 //        }
 
-        switch (fruit) {
-            case "Mango" -> System.out.println("King of fruit");
-            case "Apple" -> System.out.println("A sweet red fruit");
-            case "Orange" -> System.out.println("Round fruit");
-            case "Grapes" -> System.out.println("small fruit");
-            default -> System.out.println("Please enter a valid fruit");
-        }
+//        switch (fruit) {
+//            case "Mango" -> System.out.println("King of fruit");
+//            case "Apple" -> System.out.println("A sweet red fruit");
+//            case "Orange" -> System.out.println("Round fruit");
+//            case "Grapes" -> System.out.println("small fruit");
+//            default -> System.out.println("Please enter a valid fruit");
+//        }
 
 
+
+
+
+        int day  = input.nextInt();
+//        switch (day) {
+//            case 1:
+//            case 2:
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("Weekday");
+//                break;
+//
+//
+//            case 6:
+//            case 7:
+//                System.out.println("Weekend");
+//                break;
+        //}
+
+
+        switch (day) {
+            case 1, 2, 3, 4, 5-> System.out.println("Weekday");
+
+            case 7,8 -> System.out.println("Weekend");
+
+       }
 
 
 
