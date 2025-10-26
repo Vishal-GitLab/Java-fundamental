2697512294f6cac4363edaf7a286086cc78d0c66
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-10-26 22:52:09 +0530

    Add files via upload

---
+++ src/src/FunctionAndMethods/StringExample.java
@@ -0,0 +1,29 @@
+package FunctionAndMethods;
+
+import java.util.Scanner;
+
+public class StringExample {
+    public static void main(String[] args) {
+//        String msg = greet();
+//        System.out.println(msg);
+//    }
+//
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.next();
+         String personalised  = myGreet(name);
+        System.out.println(personalised);
+
+
+    }
+    static String myGreet(String name) {
+        String message = "Hello " + name;
+        return  message;
+    }
+
+
+//    static String greet() {
+//        String greeting =  "how are you";
+//        return greeting;
+//    }
+}
