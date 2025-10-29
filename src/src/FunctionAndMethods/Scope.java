e7a037d5882092d74518a68924cbd5b58fbdd739
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-10-29 23:09:37 +0530

    Add files via upload

---
+++ src/src/FunctionAndMethods/Scope.java
@@ -0,0 +1,14 @@
+package FunctionAndMethods;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a  = 20;
+        int b = 30;
+
+    }
+    static void random(int marks) {
+        int num  = 67;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
