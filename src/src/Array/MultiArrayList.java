35c826f0663b5b552e8b29121f9cbe9988c1fbc6
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-02 00:04:10 +0530

    Add files via upload

---
+++ src/src/Array/MultiArrayList.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiArrayList {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        // initialisation
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        // add element
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(input.nextInt());
+            }
+        }
+        System.out.println(list);
+    }
+}
