380bb5d58cd035f09ed262299a08f9985e03141b
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-11 23:54:46 +0530

    Add files via upload

---
+++ src/src/SelectionSort/Main.java
@@ -0,0 +1,64 @@
+package SelectionSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // int[]  arr = {5,4,3,2,1};
+//        int[] arr = {-1,7,-32,0, 89};
+//        bubble(arr);
+        int[] arr = {5,4,3,2,1};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - 1 -i;
+            int maxIndex = getMaxIndex(arr, 0,last);
+            swap(arr,maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+         arr[second] = temp;
+    }
+
+
+     static int getMaxIndex(int[]  arr, int start, int end ) {
+        int max = start;
+        for (int i = 0; i < end; i++) {
+            if (arr[max] < arr[i]){
+                max = i;
+            }
+        }
+        return max;
+    }
+
+
+    static void bubble(int[] arr) {
+        boolean swapped;
+        // run the steps n-1 time
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            // for each step, max item will come at j=the last respective index
+            for (int j = 1; j < arr.length - i; j++) {
+                // swap if the item is smaller than the previous  item
+                if (arr[j] < arr[j - 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            // if you did not swap for a particular value of i, it items the array is sorted hence stop the program
+            if (!swapped) {      // (swapped == false)            // !false =  true;
+                break;
+            }
+        }
+    }
+}
