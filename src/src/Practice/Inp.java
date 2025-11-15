9e5965517c68d4406e6fc4ff4939e68417cfb1bc
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-15 23:28:36 +0530

    Add files via upload

---
+++ src/src/Practice/Inp.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Inp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        String name = input.next();
+        System.out.println(name);
+
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
+    }
+}
