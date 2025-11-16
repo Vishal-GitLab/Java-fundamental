697bc7c86233fa99606f15d56d637edc41b28426
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-16 23:40:49 +0530

    Add files via upload

---
+++ src/src/Practice/Input.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hey how are you ");
+        int first = input.nextInt();
+        int second = input.nextInt();
+    }
+}
