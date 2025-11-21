ef7dcb0fa7fa5cfd44b7e7f78ff7641d16148dff
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-21 23:40:54 +0530

    Add files via upload

---
+++ src/src/Practice/Primitives.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNumber =  34;
+
+
+        char c = 'A';
+        double d = 3335323.3534;
+        float f = 34.56f;
+        long largeInteger = 345454075;
+        boolean check = false;
+
+    }
+}
