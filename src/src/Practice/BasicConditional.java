b1733ebc9ae93e3725deb502823f5a5dffc8865d
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-22 13:40:47 +0530

    Add files via upload

---
+++ src/src/Practice/BasicConditional.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class BasicConditional {
+    public static void main(String[] args) {
+        for (int count = 0; count <= 5; count++) {
+            System.out.println(count);
+        }
+    }
+}
