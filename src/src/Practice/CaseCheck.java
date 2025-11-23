e8f9f27303262612b57b3a665236a0545a1aed4a
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-23 22:40:09 +0530

    Add files via upload

---
+++ src/src/Practice/CaseCheck.java
@@ -0,0 +1,18 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char ch= input.next().trim().charAt(0);
+
+
+        if ( ch >= 'a' && ch <= 'z' ) {
+            System.out.println("Lower case");
+        }
+        else {
+            System.out.println("Upper case");
+        }
+    }
+}
