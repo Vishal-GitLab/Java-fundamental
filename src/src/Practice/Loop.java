79021e93a9a7c40c601bd248041c5e4c4317b5be
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-11-27 21:59:10 +0530

    Add files via upload

---
+++ src/src/Practice/Loop.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int k = 1;
+        do {
+            System.out.println(k);
+            k++;
+        }  while (k <= 10);
+    }
+}
