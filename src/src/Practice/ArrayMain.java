f51efcc632e175cbbcbf39a761566ae4b409011e
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-03 23:31:29 +0530

    Add files via upload

---
+++ src/src/Practice/ArrayMain.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+
+        int a = 45;
+
+        String name = "Ragini Chaurasiya ";
+
+        int rno1 =  12;
+        int  rno2  = 23;
+        int rno3 = 45;
+
+
+        int[] rows;
+        rows = new int[5];
+
+        String[] arr =  new String[4];
+        System.out.println(arr[0]);
+    }
+}
