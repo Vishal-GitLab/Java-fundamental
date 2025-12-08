254dee6359dbf3c1b7a8145f345d930330efe8a9
St. Vishal Chaurasiya <vishal_246045@saitm.ac.in>
2025-12-08 22:49:06 +0530

    Add files via upload

---
+++ src/src/Practice/FunctionGreeting.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class FunctionGreeting {
+    public static void main(String[] args) {
+                 greeting();
+                 }
+    static void greeting() {
+        System.out.println("Hello Vishal ");
+    }
+}
